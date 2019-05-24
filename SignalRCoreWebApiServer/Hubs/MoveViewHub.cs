ed39a91956b0037ef9b5413e3f6c14a3679ec9bb
Igor Gomes <igor.gomes@ubus.com.br>
2019-05-24 11:08:03 -0300

    -Initial Commit

---
+++ SignalRCoreWebApiServer/Hubs/MoveViewHub.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+
+namespace SignalRCoreWebApiServer.Hubs
+{
+    public class MoveViewHub : Hub
+    {
+        public async Task MoveViewFromServer(float newX, float newY)
+        {
+            Console.WriteLine($"Receive position from Server app: {newX}/{newY}");
+
+            await Clients.Others.SendAsync("ReceiveNewPosition", newX, newY);
+        }
+    }
+}
---
+++ SignalRCoreXamarinClient/MainActivity.cs
@@ -0,0 +1,88 @@
+﻿using Android.OS;
+using Android.App;
+using Android.Views;
+using Android.Widget;
+using Android.Support.V7.App;
+using static Android.Views.View;
+using Microsoft.AspNetCore.SignalR.Client;
+
+namespace SignalRCoreXamarinClient
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity, IOnTouchListener
+    {
+        Button btn_start;
+        View view_move;
+
+        HubConnection hubConnection;
+
+        public bool OnTouch(View v, MotionEvent e)
+        {
+            view_move.SetX(e.RawX);
+            view_move.SetY(e.RawY);
+
+            if (hubConnection.State == HubConnectionState.Connected)
+            {
+                hubConnection.SendAsync("MoveViewFromServer", e.RawX, e.RawY);
+            }
+
+            return true;
+        }
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+
+            btn_start = FindViewById<Button>(Resource.Id.btn_start);
+            view_move = FindViewById<View>(Resource.Id.view_move);
+
+            //"IP Url" generated/proxy by "Conveyor" plugin (https://bit.ly/2pHjn2u) to access Hub from emulators and devices...
+            hubConnection = new HubConnectionBuilder().WithUrl("http://192.168.15.18:45455/movehub").Build();
+
+            hubConnection.On<float, float>("ReceiveNewPosition", (newX, newY) =>
+            {
+                view_move.SetX(newX);
+                view_move.SetY(newY);
+            });
+
+            btn_start.Click += async delegate
+            {
+                if (btn_start.Text.ToLower().Equals("start"))
+                {
+                    if (hubConnection.State == HubConnectionState.Disconnected)
+                    {
+                        try
+                        {
+                            await hubConnection.StartAsync();
+                            btn_start.Text = "STOP";
+                        }
+                        catch (System.Exception ex)
+                        {
+
+                        }
+                    }
+                }
+                else if (btn_start.Text.ToLower().Equals("stop"))
+                {
+                    if (hubConnection.State == HubConnectionState.Connected)
+                    {
+                        try
+                        {
+                            await hubConnection.StopAsync();
+                            btn_start.Text = "START";
+                        }
+                        catch (System.Exception)
+                        {
+
+                        }
+                    }
+                }
+            };
+
+            view_move.SetOnTouchListener(this);
+        }
+    }
+}
